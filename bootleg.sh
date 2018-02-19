f014357b26a8c8a29a707cf1c72e105401de381d
dino_0.o <35248094+dinosnore1@users.noreply.github.com>
2018-02-19 18:07:21 +0530

    Create bootleg.sh

---
+++ bootleg.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/dinosnore1/device_xiaomi_kenzo -b o8.1 device/xiaomi/kenzo
+git clone https://github.com/dinosnore1/device_xiaomi_msm8956-common -b o8.1 device/xiaomi/msm8956-common
+git clone https://github.com/dinosnore1/kernel_xiaomi_msm8956 -b 8.1 kernel/xiaomi/msm8956
+git clone https://github.com/dinosnore1/proprietary_vendor_xiaomi -b 8.1 vendor/xiaomi
+rm -rf hardware/qcom/audio-caf/msm8952
+rm -rf hardware/qcom/display-caf/msm8952
+rm -rf hardware/qcom/media-caf/msm8952
+git clone https://github.com/Cardinal-AOSP/hardware_qcom_audio -b oreo-mr1-caf-8952 hardware/qcom/audio-caf/msm8952
+git clone https://github.com/Cardinal-AOSP/hardware_qcom_media -b oreo-mr1-caf-8952 hardware/qcom/media-caf/msm8952
+git clone https://github.com/Cardinal-AOSP/hardware_qcom_display -b oreo-mr1-caf-8952 hardware/qcom/display-caf/msm8952
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_external_bson -b cm-14.1 external/bson
