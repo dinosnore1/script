845d296701b9f5ecb5bc30a71a960211e3731f58
dino_0.o <35248094+dinosnore1@users.noreply.github.com>
2018-03-17 17:09:46 +0530

    Update bootleg.sh

--- bootleg.sh
+++ bootleg.sh
@@ -8,5 +8,5 @@ rm -rf hardware/qcom/media-caf/msm8952
 git clone https://github.com/Cardinal-AOSP/hardware_qcom_audio -b oreo-mr1-caf-8952 hardware/qcom/audio-caf/msm8952
 git clone https://github.com/Cardinal-AOSP/hardware_qcom_media -b oreo-mr1-caf-8952 hardware/qcom/media-caf/msm8952
 git clone https://github.com/Cardinal-AOSP/hardware_qcom_display -b oreo-mr1-caf-8952 hardware/qcom/display-caf/msm8952
-git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.0 packages/resources/devicesettings
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-15.1 packages/resources/devicesettings
 git clone https://github.com/LineageOS/android_external_bson -b cm-14.1 external/bson
