f3b86950d2701ede76b31c819b1ae06350ce3b63
りﾉ刀の <35248094+dinosnore1@users.noreply.github.com>
2018-12-19 19:29:17 +0530

    Update bootleg_beryllium.sh

--- bootleg_beryllium.sh
+++ bootleg_beryllium.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/dinosnore1/device_xiaomi_beryllium -b pasta device/xiaomi/beryllium
 git clone https://github.com/dinosnore1/device_xiaomi_sdm845-common -b pasta device/xiaomi/sdm845-common
-git clone https://github.com/dinosnore1/kernel_xiaomi_sdm845 -b lineage-16.0 kernel/xiaomi/sdm845
+git clone https://github.com/LineageOS/android_kernel_xiaomi_sdm845 -b lineage-16.0 kernel/xiaomi/sdm845
 git clone https://github.com/dinosnore1/vendor_xiaomi -b pasta vendor/xiaomi
 git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-16.0 packages/resources/devicesettings
